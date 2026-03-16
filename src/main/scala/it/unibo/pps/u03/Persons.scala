a7aac43d0fef83ceffb8870b068098fae8531449
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-03-16 17:08:10 +0100

    Extract courses from persons

---
+++ src/main/scala/it/unibo/pps/u03/Persons.scala
@@ -0,0 +1,23 @@
+package u03
+
+object Persons:
+
+  enum Person:
+    case Student(name: String, year: Int)
+    case Teacher(name: String, course: String)
+
+  object Person:
+
+    import u03.Sequences.Sequence
+    import Sequence.*
+
+    extension (p: Person)
+      def name: String = p match
+        case Student(n, _) => n
+        case Teacher(n, _) => n
+
+    extension (s: Sequence[Person])
+      def courses: Sequence[String] = flatMap(s) ( _ match
+        case Teacher(_, c) => Cons(c, Nil())
+        case _ => Nil()
+      )
---
+++ src/test/scala/it/unibo/pps/u03/PersonTest.scala
@@ -0,0 +1,25 @@
+package it.unibo.pps.u03
+
+import org.junit.*
+import org.junit.Assert.*
+
+class PersonTest:
+
+  import u03.Persons.*
+  import Person.*
+  import u03.Sequences.*
+  import Sequence.*
+
+  val student1 = Student("Mario", 1)
+  val student2 = Student("Luigi", 2)
+  val course1 = "PPS"
+  val course2 = "PCD"
+  val teacher1 = Teacher("Viroli", course1)
+  val teacher2 = Teacher("Ricci", course2)
+  val sequence = Cons(student1, Cons(teacher1, Cons(student2, Cons(teacher2, Nil()))))
+
+  @Test
+  def testCourses(): Unit =
+    val expected = Cons(course1, Cons(course2, Nil()))
+    assertEquals(expected, sequence.courses)
+    assertEquals(Nil(), Cons(student1, Cons(student2, Nil())).courses)
