911db2a8a575cc3f63b31a1027da4eb14bd90247
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2026-03-20 23:38:44 +0100

    contains method

--- src/main/scala/it/unibo/pps/u03/Sequences.scala
+++ src/main/scala/it/unibo/pps/u03/Sequences.scala
@@ -139,7 +139,10 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10, 20, 30] => true if elem is 20
      * E.g., [10, 20, 30] => false if elem is 40
      */
-    def contains[A](s: Sequence[A])(elem: A): Boolean = ???
+    @tailrec
+    def contains[A](s: Sequence[A])(elem: A): Boolean = s match
+      case Cons(h, t) => if h == elem then true else contains(t)(elem)
+      case Nil() => false
 
     /*
      * Remove duplicates from the sequence
